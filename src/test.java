4fc611f6d2aa3e74bdeca082897ccda2ff728c68
udalovvv <udalov.vitalii@gmail.com>
2024-01-14 10:59:32 +0500

    add test100000000

--- src/test.java
+++ src/test.java
@@ -2,4 +2,10 @@ public class test {
     private String name;
     private byte age;
     private String city;
+
+    public test(String name, byte age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
 }
